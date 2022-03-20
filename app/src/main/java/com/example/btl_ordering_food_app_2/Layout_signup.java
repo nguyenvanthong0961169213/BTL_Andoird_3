196908af279b167d789778a582da3214ef3ce78d
Thong_Nguyen <nguyenvanthong0961169213@gmail.com>
2022-03-21 00:22:58 +0700

    21/03/2022

---
+++ app/src/main/java/com/example/btl_ordering_food_app_2/Layout_signup.java
@@ -0,0 +1,14 @@
+package com.example.btl_ordering_food_app_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_signup);
+    }
+}
